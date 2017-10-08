16638993c0fc12f2a91b62d186fc605792dbccc8
dz6850 <971401108@qq.com>
2017-10-08 19:52:22 +0800

    nice

--- app/src/main/java/cn/edu/gdmec/myguard/m1hone/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/myguard/m1hone/HomeActivity.java
@@ -23,7 +23,7 @@ public class HomeActivity extends AppCompatActivity {
     private long mExitTime;
 
     private SharedPreferences msharedPreferences;
-//nic
+//nice
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
